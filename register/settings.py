202003a33924aa7cd31181cf9c6a42675c74e0cd
Anjali Shirure <83868215+anjalishirure1804@users.noreply.github.com>
2021-06-26 22:14:00 +0530

    Initial Commit

--- register/settings.py
+++ register/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&fim+ft$xi6@97*csqjxf&@_k0%a2*kq3iko83!nm!sht!!ct='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =True
+DEBUG =False
 
 ALLOWED_HOSTS = ['*']
 
